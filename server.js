420720d712854b4a88c30360121d945c569703b5
Muthu kumar <muthukumar24081999@gmail.com>
2025-05-01 22:05:57 +0530

    API Development in Progress

--- server.js
+++ server.js
@@ -4,14 +4,21 @@ import bodyParser from 'body-parser';
 import 'dotenv/config'
 
 import connectDB from './config/mongodb.js'
+import userRouter from './routes/userRoutes.js';
 
 const PORT = process.env.PORT || 4000;
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.use('/api/user', userRouter);
+
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-    connectDB();
+    try {
+        console.log(`Server is running on port ${PORT}`);
+        connectDB();
+    } catch (error) {
+        console.log("Error connecting to Database", error);
+    }
 })
