cb59b888a8dcc53a544cb25eb07f2181927af0b5
Muthu kumar <muthukumar24081999@gmail.com>
2025-05-01 22:20:40 +0530

    API Development in Progress

--- controllers/userController.js
+++ controllers/userController.js
@@ -58,4 +58,22 @@ const loginUser = async (req, res) => {
     }
 }
 
-export { registerUser, loginUser, }
+
+const userCredits = async (req, res) => {
+    try {
+        const { userId } = req.body;
+
+        const user = await userModel.findById(userId);
+
+        res.json({
+            success: true, 
+            credits: user.creditBalance, 
+            user: { name: user.name}
+        });
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message});
+    }
+}
+
+export { registerUser, loginUser, userCredits}
