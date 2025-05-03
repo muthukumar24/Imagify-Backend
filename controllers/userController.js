468cc4ad35bca6b0f6a9bff15f1b6b4ca2340395
Muthu kumar <muthukumar24081999@gmail.com>
2025-05-03 22:16:44 +0530

    API Development in Progress

--- controllers/userController.js
+++ controllers/userController.js
@@ -149,4 +149,37 @@ const paymentRazorpay = async (req, res) => {
     }
 }
 
-export { registerUser, loginUser, userCredits, paymentRazorpay }
+const verifyRazorPay = async (req, res) => {
+    try {
+        
+        const { razorpay_order_id } = req.body;
+
+        const orderInfo = await razorpayInstance.orders.fetch(razorpay_order_id);
+
+        if(orderInfo.status == 'paid'){
+            const transactionData = await transactionModel.findById(orderInfo.receipt)
+            if(transactionData.payment){
+                return res.json({ success: false, message: "Payment Failed" });
+            }
+
+            const userData = await userModel.findById(transactionData.userId);
+
+            const creditBalance = userData.creditBalance + transactionData.credits;
+        
+            await userModel.findByIdAndUpdate(userData._id, { creditBalance });
+        
+            await transactionModel.findByIdAndUpdate(transactionData._id, { payment: true });
+
+            res.json({ success: true, message: "Credits Added" });
+        }
+        else {
+            res.json({ success: true, message: "Payment Failed" });
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
+}
+
+export { registerUser, loginUser, userCredits, paymentRazorpay, verifyRazorPay }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { registerUser, loginUser, userCredits, paymentRazorpay } from "../controllers/userController.js";
+import { registerUser, loginUser, userCredits, paymentRazorpay, verifyRazorPay } from "../controllers/userController.js";
 import { userAuth } from "../middlewares/auth.js";
 
 
@@ -9,5 +9,6 @@ userRouter.post('/register', registerUser);
 userRouter.post('/login', loginUser);
 userRouter.get('/credits', userAuth ,userCredits);
 userRouter.post('/pay-razor', userAuth , paymentRazorpay);
+userRouter.post('/verify-razor', verifyRazorPay);
 
 export default userRouter;
